e0eb1b7b1c614f32b89ea218a8c98340204a9459
s27081-pj <s27081@pjwstk.edu.pl>
2023-07-31 18:48:56 +0200

    first commit

---
+++ src/main/java/com/manUtd/manUtdPage/ManUtdPageApplication.java
@@ -0,0 +1,13 @@
+package com.manUtd.manUtdPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManUtdPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManUtdPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manUtd/manUtdPage/ManUtdPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manUtd.manUtdPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManUtdPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
